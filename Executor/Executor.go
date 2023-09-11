ca5c13c29b473853685ac9200b0d6e255b497cf3
tofita <tofita1337@gmail.com>
2023-09-11 14:55:44 +0300

    refactor

---
+++ Executor/Executor.go
@@ -0,0 +1,29 @@
+package Executor
+
+import (
+	client "awesomeProject/Client"
+	"awesomeProject/Reader"
+	"awesomeProject/Torrent"
+	"awesomeProject/Tracker"
+)
+
+func Execute() {
+	var filePath = "InputData/Starfield.torrent"
+	file, _ := Reader.ReadFile(filePath)
+	var torrentFile *Torrent.TorrentFile
+	torrentFile = Torrent.DownloadFiles(file)
+	var peers, peerId, _ = Tracker.GetPeers(torrentFile)
+
+	torrent := client.Torrent{
+		Peers:       peers,
+		PeerID:      peerId,
+		InfoHash:    torrentFile.InfoHash,
+		Name:        torrentFile.Info.Name,
+		Length:      torrentFile.Info.Length,
+		PieceHashes: torrentFile.PieceHashes,
+		PieceLength: torrentFile.Info.PieceLength,
+		Announce:    torrentFile.Announce,
+	}
+
+	torrent.Start()
+}
--- main.go
+++ main.go
@@ -1,29 +1,9 @@
 package main
 
 import (
-	client "awesomeProject/Client"
-	"awesomeProject/Reader"
-	"awesomeProject/Torrent"
-	"awesomeProject/Tracker"
+	"awesomeProject/Executor"
 )
 
 func main() {
-	var filePath = "InputData/Starfield.torrent"
-	file, _ := Reader.ReadFile(filePath)
-	var torrentFile *Torrent.TorrentFile
-	torrentFile = Torrent.DownloadFiles(file)
-	var peers, peerId, _ = Tracker.GetPeers(torrentFile)
-
-	torrent := client.Torrent{
-		Peers:       peers,
-		PeerID:      peerId,
-		InfoHash:    torrentFile.InfoHash,
-		Name:        torrentFile.Info.Name,
-		Length:      torrentFile.Info.Length,
-		PieceHashes: torrentFile.PieceHashes,
-		PieceLength: torrentFile.Info.PieceLength,
-		Announce:    torrentFile.Announce,
-	}
-
-	torrent.Start()
+	Executor.Execute()
 }
